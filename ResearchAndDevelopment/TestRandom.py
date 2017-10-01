c5a9047d6ed76239c863cb60827f2609d304b119
kunwon1 <dave.j.moore@gmail.com>
2017-09-30 23:15:14 -0500

    misc

---
+++ ResearchAndDevelopment/TestRandom.py
@@ -0,0 +1,14 @@
+import random
+import time
+
+def repeatable_random(seed):
+    random.seed(seed)
+    return str(random.randint(1,1000000))
+
+def test():
+    while 1:
+        print(repeatable_random("23452345563456"))
+        time.sleep(1) 
+
+if __name__ == '__main__':
+    test()
